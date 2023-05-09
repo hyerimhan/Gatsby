1b0a70343277b4ce48aab9f0e6045223fd66a528
team7 <hnhyerim@gmail.com>
2023-05-09 16:05:06 +0900

    Style: 코드 정리

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,15 +1,32 @@
 import React from 'react'
-import Layout from '../components/layout'
+import Layout from '../components/Layout'
 import Seo from '../components/Seo'
+import { PageProps, graphql } from 'gatsby'
 
-const Blog = () => {
+const Blog = ({ data }: PageProps<Queries.BlogTitlesQuery>) => {
+  console.log(data)
+
   return (
     <Layout title='Blog'>
-      <p>The most recent news from my shop.</p>
+      <ul>
+        {data.allFile.nodes.map((file, index) => (
+          <li key={index}>{file.name}</li>
+        ))}
+      </ul>
     </Layout>
   )
 }
 
 export default Blog
+
+export const query = graphql`
+  query BlogTitles {
+    allFile {
+      nodes {
+        name
+      }
+    }
+  }
+`
 
 export const Head = () => <Seo title='Blog' />
