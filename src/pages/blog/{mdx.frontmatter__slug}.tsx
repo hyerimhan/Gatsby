290db4ef6a2e55a2f00ecea10141242304ac5d5e
team7 <hnhyerim@gmail.com>
2023-05-10 02:14:27 +0900

    Feat: Blog Post

--- src/pages/blog/{mdx.frontmatter__slug}.tsx
+++ src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -1,12 +1,39 @@
 import React from 'react'
 import Layout from '../../components/Layout'
+import { graphql } from 'gatsby'
+import Seo from '../../components/Seo'
 
-const BlogPost = () => {
+interface IBlogPost {
+  data: Queries.PostDetailQuery
+  children: any
+}
+const BlogPost = ({ data, children }: IBlogPost) => {
+  console.log(data)
+
   return (
-    <Layout title='Blog post'>
-      <div />
+    <Layout title=''>
+      <div>{children}</div>
     </Layout>
   )
 }
 
 export default BlogPost
+
+export const query = graphql`
+  query PostDetail($frontmatter__slug: String) {
+    mdx(frontmatter: { slug: { eq: $frontmatter__slug } }) {
+      body
+      frontmatter {
+        author
+        category
+        date
+        slug
+        title
+      }
+    }
+  }
+`
+
+export const Head = ({ data }: IBlogPost) => (
+  <Seo title={data.mdx?.frontmatter?.title!} />
+)
