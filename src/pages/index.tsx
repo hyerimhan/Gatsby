39c51d479248f2340c74a97d06ba8e3b65f20f92
team7 <hnhyerim@gmail.com>
2023-05-13 01:42:37 +0900

    Feat: Contentful

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,41 @@
 import React from 'react'
 import Layout from '../components/Layout'
 import Seo from '../components/Seo'
-import { StaticImage } from 'gatsby-plugin-image'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+import { PageProps, graphql } from 'gatsby'
 
-const IndexPage = () => {
+const IndexPage = ({ data }: PageProps<Queries.StickersQuery>) => {
   return (
     <Layout title='Welcome to DevStickers 🖐🏻'>
-      <StaticImage
-        height={200}
-        src='https://images.unsplash.com/photo-1625768376503-68d2495d78c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1025&q=80'
-        alt='Stickers on the wall'
-      />
+      {data.allContentfulStickerPack.nodes.map((sticker) => (
+        <article>
+          <GatsbyImage
+            image={getImage(sticker.preview?.gatsbyImageData!)!}
+            alt={sticker.name!}
+          />
+          <h2>{sticker.name}</h2>
+          <h4>${sticker.price}</h4>
+        </article>
+      ))}
     </Layout>
   )
 }
 
 export default IndexPage
+
+export const query = graphql`
+  query Stickers {
+    allContentfulStickerPack {
+      nodes {
+        name
+        price
+        preview {
+          gatsbyImageData(placeholder: BLURRED, height: 250)
+        }
+      }
+    }
+  }
+`
 
 // Head 컴포넌트는 html의 'head'와 같은 것을 의미한다.
 export const Head = () => <Seo title='Home' />
